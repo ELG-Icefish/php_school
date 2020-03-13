f1d48adc0e0b4bfce8ab26ae5772317ea05e386f
ELG-Icefish <56534936+ELG-Icefish@users.noreply.github.com>
2020-03-13 08:41:36 +0100

    Add files via upload

---
+++ projekt_ikt.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>;
+<html>
+<head>
+  <meta charset=\"utf-8\">
+  <title>Online Shop</title>
+  <?php
+  $mysqli = new mysqli('localhost','root','','shop');
+  if ($mysqli->connect_error) {
+  echo "Fehler bei der Verbindung:". msqli_connect_error();
+  exit();
+  }
+  else {
+  echo "Verbunden<br>";
+  }
+  ?>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
