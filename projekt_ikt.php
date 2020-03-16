89af6bcf9efa8455b78b5258a8c7fab577a29e5c
ELG-Icefish <56534936+ELG-Icefish@users.noreply.github.com>
2020-03-16 13:55:34 +0100

    Add files via upload

--- projekt_ikt.php
+++ projekt_ikt.php
@@ -5,6 +5,27 @@
   <meta charset=\"utf-8\">
   <title>Online Shop</title>
   <?php
+  $cookie = "user";
+  if(isset($_COOKIE[$cookie])) {
+    echo "Sie sind als $_COOKIE[$cookie] angemeldet";
+    echo "<br>";
+    echo '<a href="http://localhost/onlineshop/shop.php">Weiter</a>';
+    echo "<br>";
+    echo'Benutzer wechseln:<br>';
+    echo '
+    <form method="post" action="script.php">
+	   <input type="submit" name="sent">
+     </form>';
+     $cookie_user= $_COOKIE[$cookie];
+     if(isset($_POST['sent'])){
+       setcookie ($cookie, "$cookie_user", time() -1);
+       echo'Bitte neu laden!';
+ }
+
+
+  }
+  else {
+
   $mysqli = new mysqli('localhost','root','','shop');
   if ($mysqli->connect_error) {
   echo "Fehler bei der Verbindung:". msqli_connect_error();
@@ -14,51 +35,75 @@
   echo "Verbunden<br>";
   }
   ?>
-</head>
-<body>
-<h1>Login</h1>
-  <form action="shop.php" id="formular" method="post">
-		Benutzer:<br>
-		<input name="username" type="text"><br>
-		Password:<br>
-	  <input name="password" type="text"><br>
+  <?php
+  if ((empty($_POST["username"]))||(empty($_POST["password"]))) {
+echo ';</head>';
+echo'<body>';
+echo'<h1>Login</h1>';
+echo'<form action="shop.php" id="formular" method="post">';
+echo'Benutzer:<br>';
+echo'<input name="username" type="text"><br>';
+echo'Password:<br>';
+echo'<input name="password" type="text"><br>';
 
-		<input type="submit">
-	</form>
+echo'<input type="submit">';
+echo'</form>';
+}
+else {
 
 
 
+  $query = 'SELECT b_password FROM benutzer WHERE b_username = ?';
+$benutzer = htmlspecialchars($_POST['username']);
+
+if($stmt = $mysqli->prepare($query)){
+   /*
+        Binds variables to prepared statement
+
+        i    corresponding variable has type integer
+        d    corresponding variable has type double
+        s    corresponding variable has type string
+        b    corresponding variable is a blob and will be sent in packets
+   */
+   $stmt->bind_param('s',$benutzer);
+
+   /* execute query */
+   $stmt->execute();
+
+   /* Store the result (to get properties) */
+   $stmt->store_result();
 
 
-<?php
-  $benutzer = 0;
-  $passwort1 = 0;
-  $passwort2 =0;
-  $benutzer = htmlspecialchars($_POST['username']);
-  $passwort1 = htmlspecialchars($_POST['password']);
-  //echo "$benutzer";
-  //echo "$passwort1";
-  $sql = "SELECT b_password
-  from benutzer
-  where b_usename LIKE '$benutzer' ";
 
-  if ($stmt = $mysqli->prepare($sql)) {
+   /* Bind the result to variables */
+   $stmt->bind_result($passwort2);
 
-  $stmt->execute();
+   while ($stmt->fetch()) {
 
-  $stmt->bind_result($passwort2);
-  $stmt->close();
+
+   }
+
+   /* free results */
+   $stmt->free_result();
+
+   /* close statement */
+   $stmt->close();
 }
 
-
-
+$passwort1 = htmlspecialchars($_POST['password']);
 if ("$passwort1"=="$passwort2") {
   echo "erfolgreich";
+$cookie = "user";
+setcookie($cookie, $benutzer, "/");
+header('location: http://localhost/onlineshop/shop.php');
+exit(1);
 }
 else {
   echo "Passwort falsch";
 }
 
+}
+}
 ?>
 </body>
 </html>
---
+++ shop.php
@@ -0,0 +1,4 @@
+<?php
+$cookie = "user";
+echo $_COOKIE[$cookie];
+?>
