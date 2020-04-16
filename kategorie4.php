03e160713360486cf4e52be77f6c287d3e3ff02f
ICEFISH98 <45043720+ICEFISH98@users.noreply.github.com>
2020-04-16 09:40:53 +0200

    ...

---
+++ kategorie4.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+<head>
+  <?php
+ $benutzer=1;
+   $cookie = "user"; $benutzer=$_COOKIE[$cookie];
+
+
+  include 'qry.php'; ?>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+</head>
+<body>
+
+<h1>Kategorie4</h1><br>
+<table>
+  <tr>
+    <td>
+      <img src="bilder/placeholder.png" alt="placeholder" width="200" height="200">
+      <p>produkt10<p>
+        <br>
+      <p>
+        <table>
+          <tr>
+            <td>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+      	   <input type="submit" value="+" name="plus">
+           </form>
+         </td>
+            <td>
+             <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+         	   <input type="submit" value="-" name="minus">
+              </form></td>
+          </tr>
+     </table>
+      <?php
+        $produkt="e_p10";
+        if(isset($_POST['plus'])){
+          plus($benutzer, $produkt);
+        }
+
+        if(isset($_POST['minus'])){
+          minus($benutzer, $produkt);
+        }
+         ?>
+
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="bilder/placeholder.png" alt="placeholder" width="200" height="200">
+      <p>produkt11<p>
+        <br>
+      <p>
+        <table>
+          <tr>
+            <td>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+      	   <input type="submit" value="+" name="plus2">
+           </form>
+         </td>
+            <td>
+             <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+         	   <input type="submit" value="-" name="minus2">
+              </form></td>
+          </tr>
+     </table>
+      <?php
+        $produkt="e_p11";
+        if(isset($_POST['plus2'])){
+          plus($benutzer, $produkt);
+        }
+
+        if(isset($_POST['minus2'])){
+          minus($benutzer, $produkt);
+        }
+         ?>
+
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  </tr>
+  <tr>
+    <td>
+      <img src="bilder/placeholder.png" alt="placeholder" width="200" height="200">
+      <p>produkt12<p>
+        <br>
+      <p>
+        <table>
+          <tr>
+            <td>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+      	   <input type="submit" value="+" name="plus3">
+           </form>
+         </td>
+            <td>
+             <form method="post" action="<?php echo htmlspecialchars($_SERVER ['PHP_SELF']); ?>">
+         	   <input type="submit" value="-" name="minus3">
+              </form></td>
+          </tr>
+     </table>
+      <?php
+        $produkt="e_p12";
+        if(isset($_POST['plus3'])){
+          plus($benutzer, $produkt);
+        }
+
+        if(isset($_POST['minus3'])){
+          minus($benutzer, $produkt);
+        }
+         ?>
+
+    </td>
+    <td>
+    </td>
+    <td>
+    </td>
+  </tr>
+</table>
+
+</body>
+</html>
