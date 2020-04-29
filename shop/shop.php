ebfd366585c54e33c7c5b1ab46570b6dfb027b25
ICEFISH98 <45043720+ICEFISH98@users.noreply.github.com>
2020-04-29 14:04:59 +0200

    css kommentare!!!!

--- shop/shop.php
+++ shop/shop.php
@@ -17,18 +17,18 @@ if(isset($_COOKIE[$cookie])) {}
 
 <li><a href="start.php"  target="main">Home</a></li>
 <li><a href="warenkorb.php"  target="main">Warenkorb</a></li>
-<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Kategorien</a>
-  <div class="dropdown-content">
+<li class="liste">
+  <a href="javascript:void(0)" class="liste_btn">Kategorien</a>  <!--  javascript:void(0)= leerer link für hover  -->
+  <div class="liste-content">
     <a href="../kategorien/kategorie1.php" target="main">Obst und Gemüse</a>
     <a href="../kategorien/kategorie2.php" target="main">Gebäck</a>
     <a href="../kategorien/kategorie3.php" target="main">Kategorie3</a>
     <a href="../kategorien/kategorie4.php" target="main">Kategorie4</a>
   </div>
 </li>
-<li class="right dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Account</a>
-  <div class="dropdown-content">
+<li class="right liste">
+  <a href="javascript:void(0)" class="liste_btn">Account</a>
+  <div class="liste-content">
     <a href="../index.php" target="_parent">Abmelden</a>
     <a href="../benutzer/loeschen.php" target="_parent">Account löschen</a>
   </div>
